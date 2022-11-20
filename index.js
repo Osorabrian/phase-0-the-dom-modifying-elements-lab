99872268e1e0d43246b6639277ad1558b1e09c58
Osorabrian <osorabrian@gmail.com>
2022-11-20 06:29:10 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+const main = document.getElementById("main")
+main.remove()
+
+const newHeader = document.createElement("h1")
+newHeader.setAttribute('id', 'victory')
+
+const textNode = document.createTextNode('Brian Isaboke is the champion')
+newHeader.appendChild(textNode)
